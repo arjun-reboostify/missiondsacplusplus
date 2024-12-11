7ea7e714b79f9eedb9fa4c6df58ca1629a155de2
arjun-reboostify <reboostify@gmail.com>
2024-12-11 12:01:33 +0530

    d

--- c++.cpp
+++ c++.cpp
@@ -9,9 +9,36 @@ Node* next;
 void printlist(Node*n){
     while(n!=NULL){
 cout<<n->value<<endl;
-n->next;
+n=n->next;
     };
 };
+void insertatfront(Node**head , int newvalue){
+Node* newnode = new Node();
+newnode->value=newvalue;
+newnode->next=*head;
+*head=newnode;
+};
+void insertatend(Node**head,int newvalue){
+Node* newnode = new Node();
+newnode->value=newvalue;
+if(*head ==NULL){
+    *head=newnode;
+};
+Node* last=*head;
+while(last->next!=NULL){
+    last=last->next;
+}
+last->next=newnode;
+};
+void insertafter(Node*previous,int newvalue){
+if(previous==NULL){
+    cout<<"it can\'t be that" ;
+}
+Node* newnode = new Node();
+newnode->value=newvalue;
+newnode->next=previous->next;
+previous->next=newnode;
+}
 int main(){
     Node* head = new Node();
     Node* second = new Node();
@@ -22,6 +49,10 @@ int main(){
     second->next=third;
     third->value=4;
     third->next=NULL;
+    insertatfront(&head , 5);
+    insertatend(&head,-2);
+    insertafter(second,5);
     printlist(head);
+    
     system("pause>0");
 }
