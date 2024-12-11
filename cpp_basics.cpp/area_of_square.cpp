d35204c93d046ee9f7957b522fec3d46cc1a2e5e
amanpandey3006 <aman34pandey@gmail.com>
2024-12-11 11:36:04 +0530

    2nd program done

---
+++ cpp_basics.cpp/area_of_square.cpp
@@ -0,0 +1,12 @@
+/*To find the area of a square*/
+
+#include<iostream>
+
+int main(){
+    int a,b;
+    std::cout<<"Enter the side of the sqaure\n";
+    std::cin>>a;
+    b=a*a;
+    std::cout<<"The area of the square is:"<<b<<std::endl;
+    return 0;
+}
